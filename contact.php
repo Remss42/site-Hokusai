7f2d422a901e86c098ab608868ca3028b4183415
Remss42 <122833883+Remss42@users.noreply.github.com>
2023-06-05 18:08:25 +0200

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,16 +1,26 @@
+
+
 <?php
 //get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "remi.oms42@gmail.com";
-$subject = "Mail From website Video game";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$message = $_POST['message'] ?? '';
+
+if (!empty($email)) {
+    $to = "remi.oms42@gmail.com";
+    $subject = "Mail From website Video game";
+    $txt = "Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+    $headers = "From: noreply@yoursite.com" . "\r\n" .
+               "CC: somebodyelse@example.com";
+
+    if (mail($to, $subject, $txt, $headers)) {
+        //redirect
+        header("Location:index.html");
+        exit;
+    } else {
+        echo "Failed to send email.";
+    }
+} else {
+    echo "Email address is required.";
 }
-//redirect
-header("Location:Hokusai.html");
 ?>
